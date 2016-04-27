1e76060afb07599a1a3839c2c7380f90ba6fe3fc
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-27 15:54:57 -0400

    Added ability to flip between XO and turns off event when clicked

--- ttt.js
+++ ttt.js
@@ -1,8 +1,17 @@
-$( document ).on( 'ready', function() {
+$(document).on('ready', function() {
 
-  $( 'td' ).on( 'click', function() {
-    $( this ).html( 'X' );
-  })
+    var turn = 0;
+
+        $('td').on('click', function() {
+            if ( turn % 2 === 0 ) {
+            $(this).html('X');
+          } else {
+            $(this).html('O');
+          }
+
+          $(this).off();
+          turn++
+        })
 
 
 
